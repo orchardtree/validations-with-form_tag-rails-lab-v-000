2fec0890a968a25c9bc37e04e7b866db8440ba40
Jesse Wenger <wengerj@gmail.com>
2021-04-29 20:41:29 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,7 +8,7 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    params
+    binding.pry
     @author = Author.new(author_params)
     if @author.valid?
       @author.save
